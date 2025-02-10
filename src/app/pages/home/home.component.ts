15d002e121bd5512ebb8ae456b68f34be3807739
Alecit13 <u20221c488@upc.edu.pe>
2025-02-10 12:26:41 -0500

    feat: add second section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from '../../shared/navbar/navbar.component';
+import {NgForOf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  sections = [
+    {
+      title: 'Destinations',
+      description: 'We can arrange remarkable travel experiences on all seven continents. Where do you want to go?',
+      image: 'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp',
+      link: '/destinations'
+    },
+    {
+      title: 'Journeys',
+      description: 'Find your dream journey, whether you want to travel privately or with a group, by train, boat, or plane.',
+      image: 'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp',
+      link: '/journeys'
+    },
+    {
+      title: 'Stays',
+      description: 'Our collection of inspiring places to stay includes safari lodges, super-villas, and boutique riverboats.',
+      image: 'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp',
+      link: '/stays'
+    }
+  ];
 }
