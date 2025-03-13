35b102c868e5901a73758bef30ecf29dcf91e980
Alecit13 <u20221c488@upc.edu.pe>
2025-03-13 12:31:31 -0500

    fix: list-packages of trekking

--- src/app/pages/premium-services/services/trekking/list-packages/list-packages.component.ts
+++ src/app/pages/premium-services/services/trekking/list-packages/list-packages.component.ts
@@ -9,6 +9,7 @@ import {
 import {
     PackageListToDestinationsComponent
 } from "../../../../../shared/components/To Destinations/list-destinations/package-list-to-destinations/package-list-to-destinations.component";
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-list-packages',
@@ -22,31 +23,28 @@ import {
   styleUrl: './list-packages.component.css'
 })
 export class ListPackagesComponent {
-  images = [
-    { title: 'Private Jet Journeys', description: 'Iconic Wonders: Around the World by Private Jet', image: 'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' },
-    { title: 'Tailormade Journeys', description: 'Climb Mount Kilimanjaro', image: 'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' }
-  ];
-  info = {
-    image_principal: 'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp',
-    titlePage: 'Trekking',
-    button: 'View all packages',
-    titleInfo: 'Trekking in Peru',
-    descripcion: 'We offer the best charters to Peru. We have been spinning dreams into remarkable adventures for discerning travellers for more than 60 years, ever since our founder pioneered the first modern luxury safari in Africa. Today we are the world’s leading travel company, taking guests to the earth’s wildest frontiers on all seven continents.',
-    subtitulo: 'Best Ways to Travel',
-    text:'We have been spinning dreams into remarkable adventures for over 60 years.',
-    image_middle:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp'
-  }
-  paquetes=[
-    {nombre:'Ancascocha Trail de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:1 , ruta:'/premium-services/trekking'},
-    {nombre:'Salkantay de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:2 , ruta:'/premium-services/trekking'},
-    {nombre:'Camino Inca de Lujo Plus',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel' ,id:3 , ruta:'/premium-services/trekking'},
-    {nombre:'Qelqanqa Inca Trail de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:4 , ruta:'/premium-services/trekking'},
-    {nombre:'Choquequirao de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:5 , ruta:'/premium-services/trekking'},
-    {nombre:'Lares Trail de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:6 , ruta:'/premium-services/trekking'},
-    {nombre:'Camino Inca de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:7 , ruta:'/premium-services/trekking'},
-    {nombre:'Quarry Trail de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:8 , ruta:'/premium-services/trekking'},
-    {nombre:'Choquequirao de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:9 , ruta:'/premium-services/trekking'},
-    {nombre:'Camino Inca de Lujo',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', descripcion:'Best Ways to Travel', id:10 , ruta:'/premium-services/trekking'},
+  images: any[] = [];
+  info: any = {};
+  paquetes: any[] = [];
+  ruta = "/premium-services/premium-tours"; // La ruta se mantiene igual para todos los paquetes
 
-  ]
+  constructor(private translate: TranslateService) {
+    this.loadTranslations();
+  }
+
+  loadTranslations() {
+    this.translate.get('LIST_PACKAGES.TREKKING').subscribe((data: any) => {
+      this.images = data.images || [];
+      this.info = data.info || {};
+
+      this.paquetes = (data.paquetes || []).map((paquete: any, index: number) => ({
+        id: paquete.id || index + 1, // Si el paquete ya tiene un ID, lo mantiene. Si no, se le asigna uno basado en el índice.
+        ...paquete,
+        ruta: `${this.ruta}` // Agregar ID a la ruta.
+      }));
+    });
+
+    console.log(this.paquetes);
+  }
+
 }
