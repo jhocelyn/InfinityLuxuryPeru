cecb25d12f4ad38bbf60ed00dd1de6a97b8d56ed
Alecit13 <u20221c488@upc.edu.pe>
2025-03-13 14:49:47 -0500

    fix: list-packages of tours

--- src/app/pages/premium-services/list-services/list-services.component.ts
+++ src/app/pages/premium-services/list-services/list-services.component.ts
@@ -1,31 +1,37 @@
 import { Component } from '@angular/core';
 import {NgClass, NgForOf} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-list-services',
   imports: [
     NgForOf,
     NgClass,
-    RouterLink
+    RouterLink,
+    TranslatePipe
   ],
   templateUrl: './list-services.component.html',
   styleUrl: './list-services.component.css'
 })
 export class ListServicesComponent {
-  servicios=[
-    {nombre:'Luxury Packages',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp', slug:'luxury-packages' ,destacado:true},
-    {nombre:'Premium Tours',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug:'premium-tours', destacado: true},
-    {nombre:'MICE',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'mice', destacado:true},
-    {nombre:'Family best experiences',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'family-best-experiences', destacado:true},
-    {nombre:'Wedding',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'wedding', destacado:true},
-    {nombre:'Special Services',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'special-services', destacado:true},
-    {nombre:'Wellness',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'wellness', destacado:true},
-    {nombre:'Gastronomy',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'gastronomy', destacado:true},
-    {nombre:'Sustainable tourism',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'sustainable-tourism', destacado:true},
-    {nombre:'Charters',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'charters', destacado:true},
-    {nombre:'Trekking',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'trekking', destacado:true},
-    {nombre:'Donations',imagen:'assets/img/incredibly-beatiful-site-of-machu-picchu-2023-11-27-05-12-24-utc_11zon.webp' , slug: 'donations', destacado:true},
+  services: any = {}; // Aquí se guardarán los datos traducidos
+  servicesKeys: string[] = []; // Claves para iterar en la vista
 
-  ]
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.loadServices();
+    // Escuchar cambios de idioma para actualizar los datos
+    this.translate.onLangChange.subscribe(() => {
+      this.loadServices();
+    });
+  }
+
+  loadServices(): void {
+    this.translate.get('PREMIUM_SERVICES.services').subscribe((data) => {
+      this.services = data;
+      this.servicesKeys = Object.keys(this.services);
+    });
+  }
 }
